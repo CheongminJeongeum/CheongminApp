18bb308ea0038279a4f3cab0d89eeb9edcb37ff6
Sangjin Moon <moonsoo5522@naver.com>
2017-05-15 01:19:56 +0900

    상속 Activity 수정

--- app/src/main/java/sm/cheongminapp/LoginActivity.java
+++ app/src/main/java/sm/cheongminapp/LoginActivity.java
@@ -3,36 +3,20 @@ package sm.cheongminapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.os.Debug;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,12 +25,10 @@ import sm.cheongminapp.Model.Result;
 import sm.cheongminapp.Network.ApiServiceHelper;
 import sm.cheongminapp.Network.IApiService;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     // UI references.
     private AutoCompleteTextView mEmailView;
