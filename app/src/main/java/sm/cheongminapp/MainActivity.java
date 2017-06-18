10b6c73dd517c267047cdcceb39b867e6d310e5e
Sangjin Moon <moonsoo5522@naver.com>
2017-06-18 17:45:32 +0900

    자이로 데이터 예측 알고리즘 작성

--- app/src/main/java/sm/cheongminapp/MainActivity.java
+++ app/src/main/java/sm/cheongminapp/MainActivity.java
@@ -14,6 +14,12 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
     PreferenceData pref = new PreferenceData(this);
     BTConnector btc;
 
+    String[][] signFileNames = {{"action.csv", "goreum.csv", "healthy.csv"}};
+    String[] fingerFileNames = {"상황_병원.csv", "상황_회사.csv"};
+    String[][] vocaList = {{"가래", "간호사", "건강", "고름", "곪다", "기침", "몸살", "문병", "발목",
+     "복통", "빈혈", "설사", "수술", "심장마비", "아프다", "암", "약국", "열", "의사", "임신", "입원",
+      "저혈압", "주사", "퇴원", "폐렴"}, {"경력", "근무", "대리", "대리(직급)", "부장", "사원", "사장", "서류",
+       "야근", "월급", "출근", "출근", "출장", "취업", "퇴근", "회사원", "회의", "휴가"}};
+    // 레퍼런스 자이로 데이터
+    List<List<List<List<Float>>>> zairoReferenceData = new ArrayList<List<List<List<Float>>>>();
+    // 인풋 자이로 데이터
+    List<List<Float>> zairoInputData = new ArrayList<List<Float>>();
+    // 레퍼런스 손가락 데이터
+    List<List<List<List<Integer>>>> fingerReferenceData = new ArrayList<List<List<List<Integer>>>>();
+    // 인풋 손가락 데이터
+    List<List<Integer>> fingerInputData = new ArrayList<List<Integer>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,13 +92,15 @@ public class MainActivity extends AppCompatActivity {
         id = infoIntent.getStringExtra("id");
         getAndSetMode();
 
-/*
+        //loadReferenceFingersData();
+
+
         btc = new BTConnector(this);
 
         //if(mode == 0) {
         btc.checkBluetooth();
         //}
-*/
+
 
 
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -193,6 +216,69 @@ public class MainActivity extends AppCompatActivity {
         {
             backPressedTime = tempTime;
             Toast.makeText(getApplicationContext(), "한번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void loadReferenceZairoData() {
+        for(int outer = 0; outer<signFileNames.length; outer++) {
+            List<List<List<Float>>> zairoData = new ArrayList<List<List<Float>>>();
+            for (int i = 0; i < signFileNames[outer].length; i++) {
+                try {
+                    CSVReader reader = new CSVReader(
+                            new InputStreamReader(getAssets().open(signFileNames[outer][i])));
+                    String[] lines;
+                    List<List<Float>> entireCsvData = new ArrayList<List<Float>>();
+
+                    if (reader.readNext() == null) continue; // 첫줄의 레이블(x1, x2, x3...제거)
+                    while ((lines = reader.readNext()) != null) {
+                        List<Float> lineData = new ArrayList<Float>();
+                        for (int j = 0; j < lines.length; j++) {
+                            Log.d("line", lines[j]);
+                            lineData.add(Float.parseFloat(lines[j]));
+                        }
+                        entireCsvData.add(lineData);
+                    }
+                    zairoData.add(entireCsvData);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            zairoReferenceData.add(zairoData);
+        }
+    }
+
+    public void loadReferenceFingersData() {
+        // 최대 3번의 손가락 형태 변화가 있을 수 있으므로
+        for(int outer=0; outer<fingerFileNames.length; outer++) {
+            fingerReferenceData.add(new ArrayList<List<List<Integer>>>());
+            for (int i = 0; i < 3; i++) {
+                fingerReferenceData.get(outer).add(new ArrayList<List<Integer>>());
+            }
+
+            try {
+                CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open(fingerFileNames[outer])));
+                String[] lines;
+
+                while ((lines = reader.readNext()) != null) {
+                    for (int j = 1; j < lines.length; j++) {
+                        Log.d("finger", lines[j]);
+                        List<Integer> lineData = new ArrayList<Integer>();
+                        for (int k = 0; k < lines[j].length(); k++) {
+                            if (lines[j].charAt(k) == '-') {
+                                lineData.add(-1);
+                            } else {
+                                lineData.add(lines[j].charAt(k) - '0');
+                            }
+                        }
+                        fingerReferenceData.get(outer).get(j-1).add(lineData);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- app/src/main/java/sm/cheongminapp/service/BTService.java
+++ app/src/main/java/sm/cheongminapp/service/BTService.java
@@ -15,7 +15,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by user on 2017. 6. 9..
@@ -23,7 +25,12 @@ import java.util.List;
 public class BTService extends Service {
     private final IBinder myBinder = new MyLocalBinder();
 
-    String[] signFileNames = {"action.csv", "goreum.csv", "healthy.csv"};
+    String[][] signFileNames = {{"action.csv", "goreum.csv", "healthy.csv"}};
+    String[] fingerFileNames = {"상황_병원.csv", "상황_회사.csv"};
+    String[][] vocaList = {{"가래", "간호사", "건강", "고름", "곪다", "기침", "몸살", "문병", "발목",
+            "복통", "빈혈", "설사", "수술", "심장마비", "아프다", "암", "약국", "열", "의사", "임신", "입원",
+            "저혈압", "주사", "퇴원", "폐렴"}, {"경력", "근무", "대리", "대리(직급)", "부장", "사원", "사장", "서류",
+            "야근", "월급", "출근", "출근", "출장", "취업", "퇴근", "회사원", "회의", "휴가"}};
 
     BluetoothSocket mSocket = null;
     OutputStream mOutputStream = null;
@@ -35,17 +42,19 @@ public class BTService extends Service {
     String mStrDelimiter = "\n";
     char mCharDelimiter =  '\n';
 
-    List<List<Integer>> fingerList = new ArrayList<List<Integer>>();
-
     List<List<Float>> zairoOneList = new ArrayList<List<Float>>(); // 임시로 모아놓는 리스트
     List<List<Float>> zairoFourList = new ArrayList<List<Float>>(); // 임시로 모아놓는 리스트
 
-    List<List<List<Float>>> zairoReferenceData = new ArrayList<List<List<Float>>>();
+    // 레퍼런스 자이로 데이터
+    List<List<List<List<Float>>>> zairoReferenceData = new ArrayList<List<List<List<Float>>>>();
+    // 인풋 자이로 데이터
     List<List<Float>> zairoInputData = new ArrayList<List<Float>>();
+    // 레퍼런스 손가락 데이터
+    List<List<List<List<Integer>>>> fingerReferenceData = new ArrayList<List<List<List<Integer>>>>();
+    // 인풋 손가락 데이터
+    List<List<Integer>> fingerInputData = new ArrayList<List<Integer>>();
 
     int fingerData = 0;
-
-    String[] fileNames = {"action.csv", "goreum.csv", "healthy.csv"};
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -72,22 +81,65 @@ public class BTService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        for(int i=0; i<signFileNames.length; i++) {
-            try {
-                CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open(signFileNames[i])));
-                String[] lines;
-                List<List<Float>> entireCsvData = new ArrayList<List<Float>>();
+        //loadReferenceZairoData();
+        loadReferenceFingersData();
+    }
 
-                if(reader.readNext() == null) continue; // 첫줄의 레이블(x1, x2, x3...제거)
-                while((lines = reader.readNext()) != null) {
-                    List<Float> lineData = new ArrayList<Float>();
-                    for(int j=0; j<lines.length; j++) {
-                        Log.d("line", lines[j]);
-                        lineData.add(Float.parseFloat(lines[j]));
+    public void loadReferenceZairoData() {
+        for(int outer = 0; outer<signFileNames.length; outer++) {
+            List<List<List<Float>>> zairoData = new ArrayList<List<List<Float>>>();
+            for (int i = 0; i < signFileNames[outer].length; i++) {
+                try {
+                    CSVReader reader = new CSVReader(
+                            new InputStreamReader(getAssets().open(signFileNames[outer][i])));
+                    String[] lines;
+                    List<List<Float>> entireCsvData = new ArrayList<List<Float>>();
+
+                    if (reader.readNext() == null) continue; // 첫줄의 레이블(x1, x2, x3...제거)
+                    while ((lines = reader.readNext()) != null) {
+                        List<Float> lineData = new ArrayList<Float>();
+                        for (int j = 0; j < lines.length; j++) {
+                            Log.d("line", lines[j]);
+                            lineData.add(Float.parseFloat(lines[j]));
+                        }
+                        entireCsvData.add(lineData);
                     }
-                    entireCsvData.add(lineData);
+                    zairoData.add(entireCsvData);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                zairoReferenceData.add(entireCsvData);
+            }
+            zairoReferenceData.add(zairoData);
+        }
+    }
+
+    public void loadReferenceFingersData() {
+        // 최대 3번의 손가락 형태 변화가 있을 수 있으므로
+        for(int outer=0; outer<fingerFileNames.length; outer++) {
+            fingerReferenceData.add(new ArrayList<List<List<Integer>>>());
+            for (int i = 0; i < 3; i++) {
+                fingerReferenceData.get(outer).add(new ArrayList<List<Integer>>());
+            }
+
+            try {
+                CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open(fingerFileNames[outer])));
+                String[] lines;
+
+                while ((lines = reader.readNext()) != null) {
+                    for (int j = 1; j < lines.length; j++) {
+                        Log.d("line", lines[j]);
+                        List<Integer> lineData = new ArrayList<Integer>();
+                        for (int k = 0; k < lines[j].length(); k++) {
+                            if (lines[j].charAt(k) == '-') {
+                                lineData.add(-1);
+                            } else {
+                                lineData.add(lines[j].charAt(k) - '0');
+                            }
+                        }
+                        fingerReferenceData.get(outer).get(j).add(lineData);
+                    }
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -103,8 +155,11 @@ public class BTService extends Service {
         readBuffer = new byte[4096];            // 수신 버퍼.
 
         // 문자열 수신 쓰레드.
-        mWorkerThread = new Thread(new Runnable()
-        {
+        mWorkerThread = new Thread(new Runnable() {
+            public String[] prevFingerShape = {"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"};
+
+            Map<String, Integer> predictedVoca = new HashMap<String, Integer>();
+
             @Override
             public void run() {
                 // interrupt() 메소드를 이용 스레드를 종료시키는 예제이다.
@@ -141,16 +196,25 @@ public class BTService extends Service {
                                     final String data = new String(encodedBytes, "US-ASCII");
                                     readBufferPosition = 0;
 
+                                    boolean isChanged = false;
                                     if(data.charAt(0) == 'F') {
                                         //Log.d("F", data);
                                         String splitedData = data.split(":")[1];
                                         String[] datas = splitedData.split(",");
                                         List<Integer> fingers = new ArrayList<Integer>();
                                         for(int j = 0; j<datas.length; j++) {
-                                            Log.d("F", datas[j]);
-                                            Integer.parseInt(datas[j]);
+                                            if(!prevFingerShape[j].equals(datas[j])) {
+                                                prevFingerShape[j] = datas[j];
+                                                isChanged = true;
+                                            }
+                                            fingers.add(Integer.parseInt(datas[j]));
                                         }
-                                        fingerList.add(fingers);
+                                        if(isChanged) {
+                                            for(int j=0; j<fingers.size(); j++) {
+                                                Log.d("F", Integer.toString(fingers.get(j)));
+                                            }
+                                            fingerInputData.add(fingers);
+                                        }
                                     }
                                     else if(data.startsWith("1:")) {
                                         //Log.d("1", data);
@@ -159,7 +223,7 @@ public class BTService extends Service {
                                         List<Float> zairoOne = new ArrayList<Float>();
                                         for(int j = 0; j<datas.length; j++) {
                                             Log.d("1", datas[j]);
-                                            Float.parseFloat(datas[j]);
+                                            zairoOne.add(Float.parseFloat(datas[j]));
                                         }
                                         zairoOneList.add(zairoOne);
                                     }
@@ -170,12 +234,21 @@ public class BTService extends Service {
                                         List<Float> zairoFour = new ArrayList<Float>();
                                         for(int j = 0; j<datas.length; j++) {
                                             Log.d("4", datas[j]);
-                                            Float.parseFloat(datas[j]);
+                                            zairoFour.add(Float.parseFloat(datas[j]));
                                         }
                                         zairoFourList.add(zairoFour);
                                     }
                                     // 한 단어가 끝날 때
                                     else if(data.equals("stop")) {
+                                        /*
+                                            1번 자이로와 4번 자이로의 데이터를 합침.
+                                         */
+                                        for(int j=0; j<fingerInputData.size(); j++) {
+                                            for(int k=0; k<fingerInputData.get(i).size(); k++) {
+                                                Log.d("inputfinger", Integer.toString(
+                                                        fingerInputData.get(i).get(j).intValue()));
+                                            }
+                                        }
                                         int length = zairoOneList.size() > zairoFourList.size() ?
                                                 zairoFourList.size() : zairoOneList.size();
                                         for(int j=0; j<length; j++) {
@@ -191,6 +264,7 @@ public class BTService extends Service {
 
                                         String voca = prediction();
                                         // 챗액티비티에 브로드캐스트 전송
+                                        // 전역변수들 모두 초기화
                                     }
                                 }
                                 else if(b == '�') {
@@ -209,9 +283,97 @@ public class BTService extends Service {
             }
             // 예측한 단어 반환
             public String prediction() {
-                return "ㅗㅗ";
+                // 손가락에서 추리고 남은 후보군들
+                HashMap<String, Integer> noVoca = new HashMap<String, Integer>();
+
+                for(int i=0; i<fingerReferenceData.size(); i++) {
+                    // 어떤 상황이냐에 따라 패스할지 말지 결정
+                    for(int j=0; j<fingerReferenceData.get(i).size(); j++) {
+                        for(int k=0; k<fingerReferenceData.get(i).get(j).size(); k++) {
+                            // 손가락 데이터 8개 순회
+                            List<Integer> fingerInfoList = fingerReferenceData.get(i).get(j).get(k);
+                            for(int l=0; l<fingerInfoList.size(); l++) {
+                                if(fingerInfoList.get(l) == -1) continue;
+                                else if(fingerInfoList.get(l).intValue()
+                                        != fingerInputData.get(j).get(l).intValue()) {
+                                    // 안되는 단어들 처리
+                                    noVoca.put(i+"-"+k, 1);
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                for(int i=0; i<vocaList.length; i++) {
+                    // 여기서 상황 한정 결과 추리기
+                    for(int j=0; j<vocaList[i].length; j++) {
+                        if(noVoca.get(i+"-"+j) == 1) {
+                            Log.d("vocalist", vocaList[i][j]);
+                            predictedVoca.put(vocaList[i][j], 1);
+                        }
+                    }
+                }
+
+                // 자이로 데이터로 예측
+                return matching();
             }
 
+            public String matching() {
+                float minDistance = 999999999;
+                int minI = 0, minJ = 0;
+
+                for(int i=0; i<signFileNames.length; i++) {
+                    for(int j=0; j<signFileNames[i].length; j++) {
+                        if(predictedVoca.get(signFileNames[i][j]) == 1) {
+                            float res = calc(i, j);
+                            if(minDistance > res) {
+                                minDistance = res;
+                                minI = i;
+                                minJ = j;
+                            }
+                        }
+                    }
+                }
+
+                return vocaList[minI][minJ];
+            }
+
+            public float calc(int y, int x) {
+                List<List<Float>> referData = zairoReferenceData.get(y).get(x);
+                float[][] matrix = new float[referData.size()][zairoInputData.size()];
+
+                for(int i=0; i<referData.size(); i++) {
+                    for(int j=0; j<zairoInputData.size(); j++) {
+                        float result = cosineSim(referData.get(i), zairoInputData.get(j));
+                        if(i == 0 && j == 0) matrix[i][j] = result;
+                        else if(i == 0) matrix[i][j] = matrix[i][j-1] + result;
+                        else if(j == 0) matrix[i][j] = matrix[i-1][j] + result;
+                        else matrix[i][j] = Math.min(matrix[i-1][j-1], Math.min(matrix[i][j-1],
+                                    matrix[i-1][j])) + result;
+                    }
+                }
+
+                return matrix[referData.size()-1][zairoInputData.size()-1];
+            }
+
+            private float cosineSim(List<Float> referData, List<Float> inputData) {
+                float mo = 0;
+                float ja = 0;
+                float referValue = 0, inputValue = 0;
+
+                for(int i=0; i<referData.size(); i++) {
+                    ja += referData.get(i)*inputData.get(i);
+                }
+                for(int i=0; i<referData.size(); i++) {
+                    referValue += referData.get(i)*referData.get(i);
+                    inputValue += inputData.get(i)*inputData.get(i);
+                }
+                mo = (float) Math.sqrt(referValue) * (float) Math.sqrt(inputValue);
+
+                return Math.abs(1 - (ja / mo));
+            }
         });
         mWorkerThread.start();
     }
