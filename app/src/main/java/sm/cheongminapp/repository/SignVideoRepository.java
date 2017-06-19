87b31c451973774b731fafb7c39c482ec4d537d4
Raye <okps123@naver.com>
2017-06-19 21:00:31 +0900

    SignVideo Prefix 수정

--- app/src/main/java/sm/cheongminapp/repository/SignVideoRepository.java
+++ app/src/main/java/sm/cheongminapp/repository/SignVideoRepository.java
@@ -14,36 +14,35 @@ import sm.cheongminapp.data.SignData;
  */
 
 public class SignVideoRepository {
-    private static String packageName = "sm.cheongminapp";
+    private final static String filePath = "android.resource://sm.cheongminap/";
 
     private Map<String, String> signFileMap = new HashMap<>();
 
     SignVideoRepository() {
-        // TODO: DB에서 수화 매핑 정보를 오도록 수정해야함
-        // 수어 <-> 경로
-        /* MOV000250232 고맙다, 감사
-MOV000249602 기다리다, 대기
-MOV000249438 괜찮다, 무방하다
-MOV000250606 늦다, 느리다 , 지각 , 더디다 , 서서히 , 지체 , 천천하다 , 찬찬하다
-MOV000241289 말씀
-*/
-        signFileMap.put("고맙", "android.resource://" + packageName + "/" + R.raw.mov000250232);
-        signFileMap.put("감사하", "android.resource://" + packageName + "/" + R.raw.mov000250232);
+        /*
+        MOV000250232 고맙다, 감사
+        MOV000249602 기다리다, 대기
+        MOV000249438 괜찮다, 무방하다
+        MOV000250606 늦다, 느리다 , 지각 , 더디다 , 서서히 , 지체 , 천천하다 , 찬찬하다
+        MOV000241289 말씀
+        */
+        signFileMap.put("고맙", filePath + R.raw.mov000250232);
+        signFileMap.put("감사하", filePath + R.raw.mov000250232);
 
-        signFileMap.put("기다리", "android.resource://" + packageName + "/" + R.raw.mov000249602);
-        signFileMap.put("대기", "android.resource://" + packageName + "/" + R.raw.mov000249602);
+        signFileMap.put("기다리", filePath + R.raw.mov000249602);
+        signFileMap.put("대기", filePath + R.raw.mov000249602);
 
-        signFileMap.put("괜찮",  "android.resource://" + packageName + "/" + R.raw.mov000249438);
-        signFileMap.put("무방하",  "android.resource://" + packageName + "/" + R.raw.mov000249438);
+        signFileMap.put("괜찮", filePath + R.raw.mov000249438);
+        signFileMap.put("무방하", filePath + R.raw.mov000249438);
 
-        signFileMap.put("늦",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
-        signFileMap.put("느리",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
-        signFileMap.put("지각",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
-        signFileMap.put("더디",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
-        signFileMap.put("서서히",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
-        signFileMap.put("지체",  "android.resource://" + packageName + "/" + R.raw.mov000250606);
+        signFileMap.put("늦", filePath + R.raw.mov000250606);
+        signFileMap.put("느리", filePath + R.raw.mov000250606);
+        signFileMap.put("지각", filePath + R.raw.mov000250606);
+        signFileMap.put("더디", filePath + R.raw.mov000250606);
+        signFileMap.put("서서히", filePath + R.raw.mov000250606);
+        signFileMap.put("지체",  filePath + R.raw.mov000250606);
 
-        signFileMap.put("말씀",  "android.resource://" + packageName + "/" + R.raw.mov000241289);
+        signFileMap.put("말씀",  filePath + R.raw.mov000241289);
     }
 
     public SignData getSignData(String signText) {
